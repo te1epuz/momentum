c6a17727ae8ccd638d8572c3c9a1d1d6b562869b
Alexander Martinovich <alexander_martinovich@msn.com>
2022-07-07 01:58:57 +0300

    fix: change mobile images

--- travel/scripts/main.js
+++ travel/scripts/main.js
@@ -2,11 +2,25 @@
 (function () {
   const burger = document.querySelector('.header_burger');
   const menu = document.querySelector('.header_nav');
+  const menuLink = document.querySelectorAll('.header_nav_link');
   const menuCloseButton = document.querySelector('.header_nav_close');
+  const blackBg = document.querySelector('.black_bg');
+    
   burger.addEventListener('click', () => {
     menu.classList.add('header_nav_active');
-  });
-  menuCloseButton.addEventListener('click', () => {
+    blackBg.classList.add('black_bg_active');
+   });
+  
+  blackBg.addEventListener('click', () => {
     menu.classList.remove('header_nav_active');
+    blackBg.classList.remove('black_bg_active');
   });
+  
+  menuLink.forEach(link => {
+    link.addEventListener('click', () => {
+    menu.classList.remove('header_nav_active');
+    blackBg.classList.remove('black_bg_active'); 
+    });
+  });
+   
 }());
