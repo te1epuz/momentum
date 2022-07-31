08c1d49b161a2024469bd1981a0c997724950c45
Alexander Martinovich <alexander_martinovich@msn.com>
2022-07-31 16:10:50 +0300

    feat: add greeting

--- momentum/js/time.js
+++ momentum/js/time.js
@@ -1,14 +1,23 @@
 const time_block = document.querySelector('.time');
 const date_block = document.querySelector('.date');
+const greeting_block = document.querySelector('.greeting');
+const name_block = document.querySelector('.name');
+
+
 
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
   time_block.textContent = currentTime;
+
   showDate();
+  showGreeting();
+
   setTimeout(showTime, 1000);
 }
 showTime();
+
+
 
 function showDate() {
   const date = new Date();
@@ -17,3 +26,31 @@ function showDate() {
   date_block.textContent = currentDate;
 }
 
+
+
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours();
+  if (hours < 6) { return 'nigth' }
+  else if (hours >= 6 && hours < 12) { return 'morning' }
+  else if (hours >= 12 && hours < 18) { return 'afternoon' }
+  else if (hours >= 18 && hours < 24) { return 'evening' }
+}
+
+function showGreeting() {
+  greeting_block.textContent = `Good ${getTimeOfDay()},`;
+}
+
+
+function setLocalStorage() {
+  localStorage.setItem('name', name_block.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    name_block.value = localStorage.getItem('name');
+  }
+  else { name_block.placeholder = '[enter Name]'}
+}
+window.addEventListener('load', getLocalStorage)
