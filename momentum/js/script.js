36bb05885e562f8a39b201b5c08a796b81961f97
Alexander Martinovich <alexander_martinovich@msn.com>
2022-07-31 19:52:18 +0300

    feat: add background slider

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -2,8 +2,8 @@ const time_block = document.querySelector('.time');
 const date_block = document.querySelector('.date');
 const greeting_block = document.querySelector('.greeting');
 const name_block = document.querySelector('.name');
-
-
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
 
 function showTime() {
   const date = new Date();
@@ -31,7 +31,7 @@ function showDate() {
 function getTimeOfDay() {
   const date = new Date();
   const hours = date.getHours();
-  if (hours < 6) { return 'nigth' }
+  if (hours < 6) { return 'night' }
   else if (hours >= 6 && hours < 12) { return 'morning' }
   else if (hours >= 12 && hours < 18) { return 'afternoon' }
   else if (hours >= 18 && hours < 24) { return 'evening' }
@@ -54,3 +54,39 @@ function getLocalStorage() {
   else { name_block.placeholder = '[enter Name]'}
 }
 window.addEventListener('load', getLocalStorage)
+
+
+
+let randomNum = '01';
+getRandomNum();
+
+function getRandomNum() {
+  randomNum = Math.floor(Math.random() * 21).toString().padStart(2, "0");
+  return;
+}
+
+function setBg() {
+  const timeOfDay = getTimeOfDay();
+
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/Te1epuz/momentum-images/main/images/${timeOfDay}/${randomNum}.jpg`
+  img.onload = () => {      
+    body.style.backgroundImage = `url(${img.src})`;
+  }; 
+}
+setBg()
+
+function getSlideNext() {
+  randomNum = (Number(randomNum) + 1).toString().padStart(2, "0"); 
+  if (randomNum === '21') { randomNum = '01'; }
+  setBg()
+}
+
+function getSlidePrev() {
+  randomNum = (Number(randomNum) - 1).toString().padStart(2, "0"); 
+  if (randomNum === '00') { randomNum = '20'; }
+  setBg()
+}
+
+slideNext.addEventListener('click', getSlideNext)
+slidePrev.addEventListener('click', getSlidePrev) 
